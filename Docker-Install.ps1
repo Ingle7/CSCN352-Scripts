cbdc96c75dc90f849ce6df744fe09ccbc86fccab
Christopher <cingle7@liberty.edu>
2024-04-26 13:52:56 -0400

    Added the PowerShell script to install docker

---
+++ Docker-Install.ps1
@@ -0,0 +1,7 @@
+﻿#Install chocolatey
+function install-choco{
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+install-choco
+choco install docker-desktop --pre
