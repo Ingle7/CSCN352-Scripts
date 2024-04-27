be401ea7c9838b54e6ccc2a9f89ac54d6d2f5735
Christopher <cingle7@liberty.edu>
2024-04-27 15:48:11 -0400

    Added Windows server backup script

---
+++ Server-Backup.ps1
@@ -0,0 +1,11 @@
+﻿$sourcePath = "\\Server-GUI\Executeables"
+$destinationPath = "C:\Backup"
+
+$backupFolder = Join-Path -Path $destinationPath -ChildPath (Get-Date -Format "yyyyMMddHHmmss")
+
+New-Item -ItemType Directory -Path $backupFolder
+
+Copy-Item -Path $sourcePath\* -Destination $backupFolder -Recurse
+
+$backupLog = Join-Path -Path $destinationPath -ChildPath "backup.log"
+Add-Content -Path $backupLog -Value "Backup completed on $(Get-Date)"
